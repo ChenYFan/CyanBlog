06751889ff43a0bed25c0f677ca9547747bcca93
CyanFalse <chen2778754364@foxmail.com>
2023-07-26 10:53:00 +0800

    Add Version Previx before timestamp

--- prescripts/pkgpublish.js
+++ prescripts/pkgpublish.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const pkgfile = {
     "name": "cyanblog",
-    "version": new Date().getTime()
+    "version": "0.0."+new Date().getTime()
 }
 fs.writeFile('./public/package.json', JSON.stringify(pkgfile), function (err) {
     if (err) {
